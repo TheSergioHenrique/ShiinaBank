663defce88b22e4e8e5e305bafd77df67cb9c331
Sérgio Henrique <72146555+TheSergioHenrique@users.noreply.github.com>
2022-07-04 17:17:14 -0300

    Update Conta.java

--- Conta.java
+++ Conta.java
@@ -1,6 +1,20 @@
-class Conta {
-	double saldo;
-	int agencia;
-	int numero;
+public class Conta {
+	double saldo =0 ;
+	int agencia=42 ;
+	int numero=0 ;
 	String titular;
+	
+	void deposita(double valor) {
+		this.saldo += valor;
+	}
+	
+	public boolean saca(double valor) {
+		if(this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		}else{
+			System.out.println("O saldo na conta é insuficiente para esta transação!");
+			return false;
+		}
+	}
 }
